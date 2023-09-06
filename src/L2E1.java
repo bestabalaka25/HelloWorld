9026aad11905516b97be561177f1fe2219e3370d
bestabalaka <30155225@nescol.ac.uk>
2023-09-06 10:56:29 +0100

    commit

--- src/L2E1.java
+++ src/L2E1.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
 public class L2E1 {
-    public static void main(String[] args) {
-    }
-    Scanner input = new Scanner(System.in);
-    System.out.print("What is your name?"  + "\n");
-    String name = input.nextLine();
 
-    System.out.print("What is your hobby?"  + "\n");
-    String hobby = input.nextLine();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name?" + "\n");
+        String name = input.nextLine();
+
+        System.out.print("What is your hobby?" + "\n");
+        String hobby = input.nextLine();
+    }
+
 }
