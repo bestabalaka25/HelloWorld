5c8d698c85c96eb2395935417a9edd7214d234a4
bestabalaka <30155225@nescol.ac.uk>
2023-09-06 10:51:16 +0100

    commit

--- src/L2E1.java
+++ src/L2E1.java
@@ -1,19 +1,12 @@
 import java.util.*;
+
 public class L2E1 {
-
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("What is your name?"  + "\n");
-        String name = input.nextLine();
-
-        System.out.print("What is your Favourite movie?"  + "\n");
-        String movie = input.nextLine();
-
-        System.out.print("What is your city?" + "\n ");
-        String city = input.nextLine();
-
-        System.out.println("Hello my name is " + name +","  + "My favourite movie is " + movie +"," + "I live in " +  city +"," + "Nice to meet you"  +"\n");
-
-
     }
+    Scanner input = new Scanner(System.in);
+    System.out.print("What is your name?"  + "\n");
+    String name = input.nextLine();
+
+    System.out.print("What is your hobby?"  + "\n");
+    String hobby = input.nextLine();
 }
