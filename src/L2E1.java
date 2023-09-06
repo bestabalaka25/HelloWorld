f053e12f5841d53758b5e7c473700f0a57c8de43
bestabalaka <30155225@nescol.ac.uk>
2023-09-06 11:19:30 +0100

    commit

--- src/L2E1.java
+++ src/L2E1.java
@@ -9,6 +9,10 @@ public class L2E1 {
 
         System.out.print("What is your hobby?" + "\n");
         String hobby = input.nextLine();
+
+        System.out.println(name  + "loves" +  hobby +  "in their free time");
+        String lineBreak = "\n";
+        System.out.println("They find it relaxing and satisfying when they are outdoor in nature"  + hobby +"." + hobby + "makes them feel happy about life");
     }
 
 }
