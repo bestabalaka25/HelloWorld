e202a36d92d1a6e064d69a517a7b0595b5cbe6a6
30155225 <30155225@nescol.ac.uk>
2023-09-27 09:55:19 +0100

    commit

---
+++ src/L5E1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class L5E1 {
+    public static void main(String[] args){
+        for( int count=1; count<=12; count++ )
+        {
+            System.out.print(count + " ");
+        }
+
+    }
+
+}
---
+++ src/L5E2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class L5E2 {
+    public static void main(String[] args){
+        for( int count=10; count<=60; count+=5 )
+        {
+            System.out.print(count + " ");
+        }
+
+    }
+
+}
