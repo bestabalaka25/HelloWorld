66be6c8a988bb159fecfa6afacffde3964f1e627
30155225 <30155225@nescol.ac.uk>
2023-10-04 10:27:16 +0100

    commit

---
+++ src/L6E1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class L6E1 {
+    public static void main(String[] args) {
+        int n = 24;
+
+        //print all even numbers <=n
+        int even = 10;
+        while (even <= n) {
+            System.out.print(even + "  ");
+            even += 2;
+        }
+    }
+}
