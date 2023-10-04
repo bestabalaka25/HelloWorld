d50314d26c1f038641831d45c671beeacd35e75f
30155225 <30155225@nescol.ac.uk>
2023-10-04 11:39:35 +0100

    commit

--- src/L6E2.java
+++ src/L6E2.java
@@ -1,6 +1,20 @@
 import java.util.*;
 public class L6E2 {
     public static void main(String [] args) {
-
+        int option;
+        do{
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter an Option(1-4): ");
+            option = scanner.nextInt();
+            System.out.println("Option Selected: " + option);
+            if (option == 1){
+                System.out.println("Hello");
+            } else if (option == 2) {
+                //Something else
+                System.out.println("Option 2");
+            } else {
+                System.out.println("Option 3");
+            }
+        } while(option != 4);
     }
 }
