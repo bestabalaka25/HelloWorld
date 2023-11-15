d7dc77ef97dc24aaa10ede896ea43779f542f240
bestabalaka <30155225@nescol.ac.uk>
2023-11-15 01:18:02 +0000

    Commit

--- src/L9E1.java
+++ src/L9E1.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 public class L9E1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-    try  {
-        System.out.print("Enter your name: ");
-        String response = scanner.nextLine();
+        try  {
+            System.out.print("Enter your name: ");
+            String response = scanner.nextLine();
 
-        for (int i = 10; i >= 0; i--) {
-            String character = response.substring(0, 1);
-            response = response.substring(1, response.length());
-            System.out.print(character);
-    } catch (Exception e) {
+            for (int i = 10; i >= 0; i--) {
+                String character = response.substring(0, 1);
+                response = response.substring(1, response.length());
+                System.out.print(character);
+            }
+        } catch (Exception e) {
             System.out.println("I am sorry, but something went wrong");
         }
     }
